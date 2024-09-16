4cfab57d8b534a3f26ef1570fc8825c4faeaba2e
andreastande <114910149+andreastande@users.noreply.github.com>
2024-09-16 16:11:32 +0200

    Skeleton code for routing and navbar
    * feat: Routing and navbar skeleton code
    
    * fix: removed old file

---
+++ app/(tabs)/chores.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const Chores = () => {
+  return (
+    <SafeAreaView>
+      <Text>Chores</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Chores
---
+++ app/(tabs)/savings.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const Savings = () => {
+  return (
+    <SafeAreaView>
+      <Text>Savings</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Savings
