9f0c8ff310b83f59fd4ba554372e6b1612330e6a
dasb12345 <106763828+dasb12345@users.noreply.github.com>
2024-09-27 20:41:42 +0200

    feat implement home page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,17 +1,123 @@
 import AppHeader from "@/components/AppHeader"
-import { Text, View, Image } from "react-native"
+import { Text, View, Image, StyleSheet, Pressable } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
-// import FastImage from 'react-native-fast-image'
+import AntIcon from "react-native-vector-icons/AntDesign"
 
 const Home = () => {
+  function handleLastMonth(): void {
+    console.log("Last month")
+  }
+
+  function handleNextMonth(): void {
+    console.log("Next month")
+  }
+
+  function handleTransactions(): void {
+    console.log("Transactions")
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "white" }}>
       <AppHeader />
-      <Text>Home</Text>
-      <Image />
-      <Text>Balance</Text>
+      <View style={styles.container}>
+        <Image style={styles.cardImage} source={require("@/assets/images/card.png")} />
+        <Text style={styles.balanceText}>1 425 503,-</Text>
+        <View style={styles.horizontalLine} />
+        <View style={styles.budgetHeader}>
+          <Pressable onPress={handleLastMonth}>
+            <AntIcon name="left" size={25} color="#000" />
+          </Pressable>
+          <Text style={styles.monthText}>August</Text>
+          <Pressable onPress={handleNextMonth}>
+            <AntIcon name="right" size={25} color="#000" />
+          </Pressable>
+        </View>
+        <View style={styles.budget}>
+          <View style={{ ...styles.budgetPost, backgroundColor: "#1A801E30" }}>
+            <Text style={styles.budgetPostText}>Mottatt</Text>
+            <Text style={styles.budgetPostText}>500,00 kr</Text>
+          </View>
+          <View style={{ ...styles.budgetPost, backgroundColor: "#FD353550" }}>
+            <Text style={styles.budgetPostText}>Brukt</Text>
+            <Text style={styles.budgetPostText}>200,00 kr</Text>
+          </View>
+          <View style={{ ...styles.budgetPost, backgroundColor: "#CBF1F4" }}>
+            <Text style={styles.budgetPostText}>Total</Text>
+            <Text style={{ ...styles.budgetPostText, color: "green" }}>+ 300,00 kr</Text>
+          </View>
+        </View>
+        <View style={styles.horizontalLine} />
+        <Pressable style={styles.transactionButton} onPress={handleTransactions}>
+          <Text style={styles.transactionsText}>Mine Transaksjoner</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardImage: {
+    width: 360,
+    height: 250,
+    resizeMode: "contain",
+  },
+  horizontalLine: {
+    borderBottomColor: "#52D1DC",
+    borderBottomWidth: 2,
+    width: 300,
+    marginVertical: 20,
+  },
+  balanceText: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  budgetHeader: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: 20,
+  },
+  monthText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginHorizontal: 100,
+  },
+  budget: {
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+    marginTop: 15,
+    gap: 15,
+  },
+  budgetPost: {
+    paddingHorizontal: 25,
+    width: 350,
+    height: 50,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    borderRadius: 20,
+  },
+  transactionButton: {
+    padding: 10,
+    paddingHorizontal: 20,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFC5D3",
+    borderRadius: 20,
+  },
+  budgetPostText: {
+    fontSize: 15,
+  },
+  transactionsText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+})
 
 export default Home
--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,11 +1,25 @@
 import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, Pressable, Image } from "react-native"
+import Awesome5Icon from "react-native-vector-icons/FontAwesome5"
 
 const AppHeader: React.FC = () => {
+  function handleProfile(): void {
+    console.log("Profile")
+  }
+
+  function handleCoin(): void {
+    console.log("Coins")
+  }
+
   return (
     <View style={styles.header}>
-      <Text style={styles.headerText}>2387</Text>
-      <Text style={styles.headerText}>2387</Text>
+      <Pressable style={styles.coinButton} onPress={handleCoin}>
+        <Text style={styles.headerText}>2387</Text>
+        <Image style={styles.coin} source={require("@/assets/images/coin.png")} />
+      </Pressable>
+      <Pressable onPress={handleProfile}>
+        <Awesome5Icon name="user-circle" size={35} />
+      </Pressable>
     </View>
   )
 }
@@ -13,17 +27,31 @@ const AppHeader: React.FC = () => {
 const styles = StyleSheet.create({
   header: {
     height: 50,
-    backgroundColor: "#f8f8f8",
+    backgroundColor: "white",
     borderBottomWidth: 1,
     borderBottomColor: "#ddd",
     justifyContent: "space-between",
     alignItems: "center",
     flexDirection: "row",
-    paddingHorizontal: 40,
+    paddingHorizontal: 25,
   },
   headerText: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  coinButton: {
+    backgroundColor: "#52D1DC",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  coin: {
+    width: 20,
+    height: 20,
+    resizeMode: "contain",
+    marginLeft: 5,
   },
 })
 
