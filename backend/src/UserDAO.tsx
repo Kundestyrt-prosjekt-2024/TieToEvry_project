01dda7e0ce35447acf43562ecee822c47f2033fc
Jonas Wolla <wolla.jonas@gmail.com>
2024-10-10 13:53:42 +0200

    feat: add and get user

---
+++ backend/src/UserDAO.tsx
@@ -0,0 +1,43 @@
+import { db } from "@/constants/firebaseConfig"
+import "firebase/firestore"
+import { addDoc, collection, doc, Firestore, getDoc, Timestamp } from "firebase/firestore"
+
+export interface User {
+  UserID: string
+  created_at: Timestamp
+  name: string
+  passphrase: string
+  phonenumber: number
+}
+class UserDAO {
+  database: Firestore
+
+  constructor() {
+    this.database = db
+  }
+
+  addUser = async (data: User) => {
+    try {
+      await addDoc(collection(this.database, "users"), data)
+    } catch (error) {
+      console.error("Error adding document: ", error)
+    }
+  }
+
+  getUser(phonenumber: string): Promise<User> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const userDoc = await getDoc(doc(this.database, "users", phonenumber))
+        if (userDoc.exists()) {
+          resolve(userDoc.data() as User)
+        } else {
+          reject(new Error("No such document!"))
+        }
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+}
+
+export default UserDAO
