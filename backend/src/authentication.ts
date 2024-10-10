6c49cbb1b714a906c769e5f1a7409a903b66d032
Jonas Wolla <wolla.jonas@gmail.com>
2024-10-10 13:53:53 +0200

    feat: authenticator creating user

---
+++ backend/src/authentication.ts
@@ -0,0 +1,32 @@
+import { auth } from '../../constants/firebaseConfig';
+import { createUserWithEmailAndPassword} from 'firebase/auth';
+import UserDAO, { User } from './UserDAO';
+import { Timestamp } from 'firebase/firestore';
+
+const userDAO = new UserDAO();
+
+export const registerUser = async (
+  email: string,
+  password: string,
+  name: string,
+  passphrase: string,
+  phonenumber: number
+) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    const newUser: User = {
+      UserID: user.uid,
+      created_at: Timestamp.now(),
+      name: name,
+      passphrase: passphrase,
+      phonenumber: phonenumber
+    };
+
+    await userDAO.addUser(newUser);
+    console.log("User registered and added to Firestore:", newUser);
+  } catch (error) {
+    console.error("Error registering user: ", error);
+  }
+};
