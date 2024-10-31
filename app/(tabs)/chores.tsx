e3e6c196b3aaa77f5afacdf469d0530349a620b6
dasb12345 <106763828+dasb12345@users.noreply.github.com>
2024-10-31 10:44:00 +0100

    feat changed to flatlist

--- app/(tabs)/chores.tsx
+++ app/(tabs)/chores.tsx
@@ -1,21 +1,22 @@
-import AppHeader from "@/components/AppHeader";
-import { View, Text, StyleSheet, Pressable, Image, Modal, Dimensions } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Chore } from "../types/chores";
-import React from "react";
-import ChoreList from "@/components/chores/chore";
-import { ScrollView } from "react-native-gesture-handler";
-import ChoresDetailedView from "@/components/chores/choresDetailedView";
-import { HandPlatter } from "lucide-react-native";
-import Button from "@/components/ui/button";
-import Popover from "@/components/chores/chorePopover";
+import AppHeader from "@/components/AppHeader"
+import { View, Text, StyleSheet, Pressable, Image, Modal, Dimensions, FlatList } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Chore } from "../types/chores"
+import React from "react"
+import ChoreList from "@/components/chores/chore"
+import { ScrollView } from "react-native-gesture-handler"
+import ChoresDetailedView from "@/components/chores/choresDetailedView"
+import { HandPlatter } from "lucide-react-native"
+import Button from "@/components/ui/button"
+import Popover from "@/components/chores/chorePopover"
+import { render } from "react-dom"
 
-const { width, height } = Dimensions.get("window")
+const { height } = Dimensions.get("window")
 
 const Chores = () => {
-  const [viewChore, toggleView] = React.useState(false);
-  const [choreOfInterest, setChoreOfInterest] = React.useState<Chore | null>(null);
-  const [showPopover, setShowPopover] = React.useState(false);
+  const [viewChore, toggleView] = React.useState(false)
+  const [choreOfInterest, setChoreOfInterest] = React.useState<Chore | null>(null)
+  const [showPopover, setShowPopover] = React.useState(false)
 
   const chores: Chore[] = [
     {
@@ -25,7 +26,7 @@ const Chores = () => {
       description: "Vask opp alle tallerkner, glass og bestikk",
       rewardNOK: 75,
       rewardCoins: 5,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 7)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 7)),
       completed: false,
       status: "Gjennomførbar",
       assignee: "Mamma",
@@ -36,7 +37,7 @@ const Chores = () => {
       description: "Rommet ditt er rotete og må ryddes. Du vet hvordan det skal være",
       rewardNOK: 50,
       rewardCoins: 2,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 6)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 6)),
       completed: false,
       status: "Gjennomførbar",
       assignee: "Mamma",
@@ -47,7 +48,7 @@ const Chores = () => {
       description: "Du har ikke gått tur med Laila denne uken. Det kan du gjøre.",
       rewardNOK: 100,
       rewardCoins: 5,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 9)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 9)),
       completed: true,
       status: "Gjennomførbar",
       assignee: "Pappa",
@@ -58,7 +59,7 @@ const Chores = () => {
       description: "Sjarken er skitten og trenger en vask. Husk å skrubbe dekket godt! Onkel blir fornøyd.",
       rewardNOK: 200,
       rewardCoins: 15,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 4)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 4)),
       completed: false,
       status: "Gjennomførbar",
       assignee: "Pappa",
@@ -69,7 +70,7 @@ const Chores = () => {
       description: "Fesken har blitt skitten og trenger en vask. Husk å skrubbe den godt! Vi vil ikke ha skitten fesk.",
       rewardNOK: 30,
       rewardCoins: 1,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)),
       status: "Ferdig",
       completed: true,
       assignee: "Mamma",
@@ -80,7 +81,7 @@ const Chores = () => {
       description: "Du har fått lekser av lærer Saddam. Gjør de!",
       rewardNOK: 60,
       rewardCoins: 2,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)),
       completed: false,
       status: "Ferdig",
       assignee: "Pappa",
@@ -91,7 +92,7 @@ const Chores = () => {
       description: "Du har fått lekser av lærer Saddam. Gjør de!",
       rewardNOK: 60,
       rewardCoins: 2,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)),
       completed: false,
       status: "Gjennomførbar",
       assignee: "Pappa",
@@ -102,77 +103,75 @@ const Chores = () => {
       description: "Du har fått lekser av lærer Saddam. Gjør de!",
       rewardNOK: 60,
       rewardCoins: 2,
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)), 
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)),
       completed: true,
       status: "Forespurt",
       assignee: "Pappa",
     },
-  ];
-  
-  const cashMulla = {
-    mulla:14243545,
-    sphareCoin: 3245,
-  };
+  ]
 
-  const setViewChore =(chore: Chore) => {
-    setChoreOfInterest(chore);
-    toggleModal();
+  const cashMulla = {
+    mulla: 14243545,
+    sphareCoin: 3245,
+  }
+
+  const setViewChore = (chore: Chore) => {
+    setChoreOfInterest(chore)
+    toggleModal()
   }
   const toggleModal = () => {
-    toggleView((prevState) => !prevState);
+    toggleView((prevState) => !prevState)
   }
-  
-  const scrollHeight = height*0.34;
+
+  function renderChore(chore: Chore) {
+    return <ChoreList chore={chore} onClick={() => setViewChore(chore)} />
+  }
+
+  function renderTop() {
+    return (
+      <View className="bg-slate-50 rounded-2xl p-4 h-[30%]">
+        <View className="border-b border-teal-300">
+          <Text className="text-lg font-regular text-center pb-2 ">Her kan du spare med Sphare!</Text>
+        </View>
+        <View className=" flex-row justify-center items-center py-2 space-x-6">
+          <Image className="rounded-md" source={require("@/assets/images/sphare1.png")} resizeMode="contain" />
+          <View className="flex flex-col h-36 justify-between items-center ">
+            <Text className="text-3xl font-normal text-teal-500">Du har tjent:</Text>
+            <Text className="text-2xl font-light text-center">{cashMulla.mulla},-</Text>
+            <View className="flex-row items-center">
+              <Image className="w-9 h-9 rounded-md" source={require("@/assets/images/coin.png")} resizeMode="contain" />
+              <Text className="text-xl font-light">x{cashMulla.sphareCoin}</Text>
+            </View>
+          </View>
+        </View>
+        <Text className="text-lg pb-2 text-center border-b border-teal-300">Aktive gjøremål 👇</Text>
+      </View>
+    )
+  }
+
+  const scrollHeight = height * 0.34
 
   return (
     <SafeAreaView className="bg-white">
       <AppHeader />
-      <View style={{height: height}} className="h- bg-white flex flex-col px-8 py-4 space-y-2">
-        <View className="bg-slate-50 rounded-2xl p-4 h-[30%]">
-          <View className="border-b border-teal-300">
-            <Text className="text-lg font-regular text-center pb-2 ">Her kan du spare med Sphare!</Text>
-          </View>
-          <View className=" flex-row justify-center items-center py-2 space-x-6">
-              <Image
-                className="rounded-md"
-                source={require("@/assets/images/sphare1.png")}
-                resizeMode="contain"
-              />
-            <View className="flex flex-col h-36 justify-between items-center ">
-              <Text className="text-3xl font-normal text-teal-500">Du har tjent:</Text>
-              <Text className="text-2xl font-light text-center">{cashMulla.mulla},-</Text>
-              <View className="flex-row items-center">
-                <Image
-                  className="w-9 h-9 rounded-md"
-                  source={require("@/assets/images/coin.png")}
-                  resizeMode="contain"
-                />
-                <Text className="text-xl font-light">x{cashMulla.sphareCoin}</Text>
-              </View>
-            </View>
-          </View>
-        </View>
+      <View style={{ height: height }} className="h- bg-white flex flex-col px-8 py-4 space-y-2">
         <View className="flex flex-col items-center h-[40%]">
-          <Text className="text-lg pb-2 text-center border-b border-teal-300">
-            Aktive gjøremål 👇
-          </Text>
-          <View style={{height: scrollHeight}} className="mb-2 border-b-2 border-teal-300">
-            <ScrollView className="" showsVerticalScrollIndicator={false}>
-              {chores.map((chore, index) => (
-                <View key={index}>
-                  <ChoreList chore={chore} onClick={() => setViewChore(chore)} />
-                </View>
-              ))}
-            </ScrollView>
+          <View style={{ height: scrollHeight }} className="mb-2 border-b-2 border-teal-300">
+            <FlatList
+              // style={styles.requestList}
+              contentContainerStyle={{ paddingBottom: 40 }}
+              data={chores}
+              renderItem={(chore) => renderChore(chore.item)}
+              ListHeaderComponent={renderTop}
+              scrollEnabled={true}
+              // onScroll={handleScroll}
+              keyExtractor={(chore) => chore.chore}
+              showsVerticalScrollIndicator={false}
+            ></FlatList>
           </View>
           <View className="border-b border-teal-300"></View>
           {choreOfInterest && (
-            <Modal
-              visible={viewChore}
-              animationType="slide"
-              transparent={true}
-              onRequestClose={toggleModal}
-            >
+            <Modal visible={viewChore} animationType="slide" transparent={true} onRequestClose={toggleModal}>
               <View className="h-full w-full flex justify-center items-center">
                 <View className="p-4 w-full">
                   <ChoresDetailedView chore={choreOfInterest} onClick={toggleModal} />
@@ -180,7 +179,7 @@ const Chores = () => {
               </View>
             </Modal>
           )}
-        </View >
+        </View>
         <View className="h-[10%] flex flex-row justify-between">
           <View>
             <Button text="Se alle gjøremål" onClick={() => setShowPopover(true)}></Button>
@@ -190,11 +189,9 @@ const Chores = () => {
           </View>
         </View>
       </View>
-      <Popover chore={chores}
-       onClick={() => setShowPopover(false)}
-       showPopover={showPopover}/>
+      <Popover chore={chores} onClick={() => setShowPopover(false)} showPopover={showPopover} />
     </SafeAreaView>
-  );
-};
+  )
+}
 
 export default Chores
