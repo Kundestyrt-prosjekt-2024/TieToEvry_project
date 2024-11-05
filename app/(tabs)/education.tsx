6183b6d240eafd286b47477184997316a6caf217
Andreas Tande <andretan@stud.ntnu.no>
2024-11-05 17:34:27 +0100

    chore: First draft of Education page

--- app/(tabs)/education.tsx
+++ app/(tabs)/education.tsx
@@ -1,12 +1,25 @@
 import AppHeader from "@/components/AppHeader"
-import { Text } from "react-native"
+import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { WebView } from "react-native-webview"
 
 const Education = () => {
+  const widgetHTML = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <script src="https://widget.gobistories.com/gwi/6" async onload="gobi.discover()"></script>
+      </head>
+      <body>
+        <div class="gobi-collection" data-gobi-collection-id="zmorw"></div>
+      </body>
+    </html>
+  `
+
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <AppHeader />
-      <Text>Education</Text>
+      <WebView originWhitelist={["*"]} source={{ html: widgetHTML }} className="flex-1" />
     </SafeAreaView>
   )
 }
