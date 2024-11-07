d1f89a775b4b3d367504cd405c800bdf11ca8f39
Andreas Tande <andretan@stud.ntnu.no>
2024-11-07 02:47:19 +0100

    feat: Added Sphare to Education page

--- app/(tabs)/education.tsx
+++ app/(tabs)/education.tsx
@@ -1,8 +1,8 @@
-import AppHeader from "@/components/AppHeader";
-import { View, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import React from 'react';
-import { WebView } from 'react-native-webview';
+import AppHeader from "@/components/AppHeader"
+import { View, StyleSheet, Text, Image } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import React from "react"
+import { WebView } from "react-native-webview"
 
 const Education = () => {
   const htmlContent = `
@@ -18,36 +18,41 @@ const Education = () => {
       <div class="gobi-collection" data-gobi-collection-id="zmorw"></div>
       </body>
     </html>
-  `;
+  `
 
   return (
-    <SafeAreaView style={styles.safeArea}>
+    <SafeAreaView className="flex-1 bg-white relative">
       <AppHeader />
-      <View style={styles.container}>
-        <WebView 
-          originWhitelist={['*']}
-          source={{ html: htmlContent }}
-          style={styles.webview}
-          mediaPlaybackRequiresUserAction={true} // Stops auto-play on iOS
-          allowsInlineMediaPlayback={true}       // Allows inline playback on iOS
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-        />
+      <View className="flex flex-row absolute top-32" style={styles.centeredView}>
+        <View>
+          <Image className="" source={require("@/assets/images/sphare_small.png")} />
+        </View>
+        <View className="relative justify-center px-8 pb-6 items-center">
+          <Image className="" source={require("@/assets/images/figma_bubble.png")} />
+          <Text className="absolute top-7 right-9 text-sm text-black text-center">
+            Her kan du lære mer om penger og sparing!
+          </Text>
+        </View>
       </View>
+      <WebView
+        originWhitelist={["*"]}
+        source={{ html: htmlContent }}
+        className="flex-1 top-32"
+        mediaPlaybackRequiresUserAction={true} // Stops auto-play on iOS
+        allowsInlineMediaPlayback={true} // Allows inline playback on iOS
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        scrollEnabled={false}
+      />
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
+  centeredView: {
+    left: "50%",
+    transform: [{ translateX: -130 }],
   },
-  container: {
-    flex: 1,
-  },
-  webview: {
-    flex: 1,
-  },
-});
+})
 
-export default Education;
+export default Education
