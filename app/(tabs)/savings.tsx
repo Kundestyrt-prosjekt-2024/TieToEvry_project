eace1862ac0b10d82e51009dfee8dbc7054b9a3b
Andreas Tande <andretan@stud.ntnu.no>
2024-11-07 12:36:49 +0100

    fix: Fixed bug with tailwindcss and saving goals

--- app/(tabs)/savings.tsx
+++ app/(tabs)/savings.tsx
@@ -1,131 +1,139 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, SafeAreaView, Keyboard, Image, TouchableWithoutFeedback } from 'react-native';
-import ReusableBottomSheet from '@/components/ReusableBottomSheet';
-import NewGoalContent from '@/components/savings/NewGoalContent';
-import AddMoneyContent from '@/components/savings/AddMoneyContent';
-import AppHeader from '@/components/AppHeader';
-import SavingGoalCard from '@/components/savings/SavingGoalCard';
-import { useGetUserID, /**useGetSavingGoals*/ } from '@/hooks/useGetFirestoreData';
-import { collection, DocumentData, onSnapshot, query, where } from 'firebase/firestore';
-import { db } from '@/constants/firebaseConfig';
-import { SavingGoal } from '@/backend/types/savingGoal';
+import React, { useEffect, useState } from "react"
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+  Keyboard,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native"
+import ReusableBottomSheet from "@/components/ReusableBottomSheet"
+import NewGoalContent from "@/components/savings/NewGoalContent"
+import AddMoneyContent from "@/components/savings/AddMoneyContent"
+import AppHeader from "@/components/AppHeader"
+import SavingGoalCard from "@/components/savings/SavingGoalCard"
+import { useGetUserID, useGetSavingGoals } from "@/hooks/useGetFirestoreData"
+import { collection, DocumentData, onSnapshot, query, where } from "firebase/firestore"
+import { db } from "@/constants/firebaseConfig"
+import { SavingGoal } from "@/backend/types/savingGoal"
 
 const Savings = () => {
-
- 
   //States for showing/hiding bottom sheets
-  const [isNewGoalVisible, setIsNewGoalVisible] = useState(false);
-  const [isAddMoneyVisible, setIsAddMoneyVisible] = useState(false);
-  
-  const [savingGoals, setSavingGoals] = useState<SavingGoal[]>([]);
-  const [loading, setLoading] = useState(true);
-  /**
-  const { data: userID, isLoading: isUserIDLoading } = useGetUserID();
-  const userIDValue = userID ?? '';
-  const { data: savingGoals, isLoading: isSavingGoalsLoading } = useGetSavingGoals(userIDValue);
+  const [isNewGoalVisible, setIsNewGoalVisible] = useState(false)
+  const [isAddMoneyVisible, setIsAddMoneyVisible] = useState(false)
+
+  const [savingGoals, setSavingGoals] = useState<SavingGoal[]>([])
+  const [loading, setLoading] = useState(true)
+
+  const { data: userID, isLoading: isUserIDLoading } = useGetUserID()
+  const userIDValue = userID ?? ""
+  const { data: savingGoals2, isLoading: isSavingGoalsLoading } = useGetSavingGoals(userIDValue)
 
   if (isUserIDLoading || isSavingGoalsLoading) {
     return (
       <SafeAreaView className="bg-white flex-1 justify-center items-center">
         <Text>Loading...</Text>
       </SafeAreaView>
-    );
+    )
   }
- */
+
   //Functions to handle showing/hiding bottom sheets
   const handleAddMoney = () => {
-    setIsAddMoneyVisible(true);
-    dismissKeyboard();
-  };
+    setIsAddMoneyVisible(true)
+    dismissKeyboard()
+  }
 
   const handleCloseAddMoney = () => {
-    setIsAddMoneyVisible(false);
-    dismissKeyboard();
-  };
+    setIsAddMoneyVisible(false)
+    dismissKeyboard()
+  }
 
   const handleNewGoalPress = () => {
-    setIsNewGoalVisible(true);
-    dismissKeyboard();
-  };
+    setIsNewGoalVisible(true)
+    dismissKeyboard()
+  }
 
   const handleCloseNewGoal = () => {
-    setIsNewGoalVisible(false);
-    dismissKeyboard();
-  };
+    setIsNewGoalVisible(false)
+    dismissKeyboard()
+  }
 
   //Hiding keyboard
   const dismissKeyboard = () => {
-    Keyboard.dismiss();
-  };
+    Keyboard.dismiss()
+  }
 
   return (
     <SafeAreaView className="bg-white">
       <AppHeader />
       <View className="h-full">
-      <ScrollView>
+        <ScrollView>
+          {/**Top view introducing user to the page */}
+          <View className="flex-row pt-5 px-10">
+            <View>
+              <Image className="w-85 h-154" source={require("@/assets/images/sphare.png")} />
+            </View>
+            <View className="flex-col justify-end">
+              <View className="relative justify-center px-8 py-4 items-center">
+                <Image className="" source={require("@/assets/images/figma_bubble.png")} />
+                <Text className="absolute pl-2 pt-1 text-sm text-black text-center">
+                  Lag et sparemål for å komme i gang!
+                </Text>
+              </View>
+              <View className="justify-end px-8">
+                <Text className="text-2xl">Dine sparemål</Text>
+              </View>
+            </View>
+          </View>
 
-        {/**Top view introducing user to the page */}
-        <View className="flex-row pt-5 px-10">
-          <View>
-            <Image className="w-85 h-154" source={require("@/assets/images/sphare.png")} />
-          </View>
-          <View className="flex-col justify-end">
-            <View className="relative justify-center px-8 py-4 items-center">
-              <Image className="" source={require("@/assets/images/figma_bubble.png")}/>
-              <Text className="absolute pl-2 pt-1 text-sm text-black text-center">Lag et sparemål for å komme i gang!</Text>
-            </View>
-            <View className="justify-end px-8">
-              <Text className="text-2xl">Dine sparemål</Text>
-            </View>
-          </View>
-        </View>
-        
-        {/** Current savings goals displayed in cards.
-         * TODO: Saving goal data should be mapped across cards */}
+          {/** Current savings goals displayed in cards.
+           * TODO: Saving goal data should be mapped across cards */}
           <View className="flex-col items-center py-5 pb-20">
-          {savingGoals && savingGoals.length > 0 ? (
-            savingGoals.map((goal) => (
-              <SavingGoalCard key={goal.id} goal={goal} onAddMoney={handleAddMoney} />
-            ))
-          ) : (
-            <Text>Du har ingen aktive sparemål!</Text>
-          )}
+            {savingGoals && savingGoals.length > 0 ? (
+              savingGoals.map((goal) => <SavingGoalCard key={goal.id} goal={goal} onAddMoney={handleAddMoney} />)
+            ) : (
+              <Text>Du har ingen aktive sparemål!</Text>
+            )}
           </View>
-
         </ScrollView>
 
         {/**Static button to create new saving goal */}
         <View className="absolute flex-col items-center bottom-20 right-5 mb-7">
-          <TouchableOpacity
-            className="bg-[#FFC5D3] w-10 h-10 rounded-full"
-            onPress={handleNewGoalPress}
-          >
+          <TouchableOpacity className="bg-[#FFC5D3] w-10 h-10 rounded-full" onPress={handleNewGoalPress}>
             <Text className="w-10 h-10 text-center text-3xl">+</Text>
           </TouchableOpacity>
           <Text className="text-xs text-center">Nytt mål</Text>
         </View>
-
       </View>
 
       {/**Bottom sheet for creating new goal */}
-      <TouchableWithoutFeedback onPress={ () => { dismissKeyboard() } }>
-      <ReusableBottomSheet isVisible={isNewGoalVisible} onClose={handleCloseNewGoal}>
-      <NewGoalContent
-          onClose={handleCloseNewGoal}
-          //userId={userID}
-        />
-      </ReusableBottomSheet>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          dismissKeyboard()
+        }}
+      >
+        <ReusableBottomSheet isVisible={isNewGoalVisible} onClose={handleCloseNewGoal}>
+          <NewGoalContent
+            onClose={handleCloseNewGoal}
+            //userId={userID}
+          />
+        </ReusableBottomSheet>
       </TouchableWithoutFeedback>
 
       {/**Bottom sheets for adding money to a goal */}
-      <TouchableWithoutFeedback onPress={ () => { dismissKeyboard() } }>
-      <ReusableBottomSheet isVisible={isAddMoneyVisible} onClose={handleCloseAddMoney}>
-        <AddMoneyContent onClose={handleCloseAddMoney} />
-      </ReusableBottomSheet>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          dismissKeyboard()
+        }}
+      >
+        <ReusableBottomSheet isVisible={isAddMoneyVisible} onClose={handleCloseAddMoney}>
+          <AddMoneyContent onClose={handleCloseAddMoney} />
+        </ReusableBottomSheet>
       </TouchableWithoutFeedback>
-
     </SafeAreaView>
-  );
-};
+  )
+}
 
-export default Savings;
+export default Savings
