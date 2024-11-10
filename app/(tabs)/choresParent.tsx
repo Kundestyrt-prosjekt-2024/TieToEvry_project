90c53811d6d392bb426b4444e234db99474503e6
Andreas Tande <andretan@stud.ntnu.no>
2024-11-10 02:54:56 +0100

    feat: Parent can choose child to get overview of

--- app/(tabs)/choresParent.tsx
+++ app/(tabs)/choresParent.tsx
@@ -1,13 +1,55 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { View, Text, FlatList, Pressable, Image } from "react-native"
+import React, { useState } from "react"
 import { SafeAreaView } from "react-native-safe-area-context"
 import AppHeader from "@/components/AppHeader"
+import { useGetChildren, useGetUser, useGetUserID } from "@/hooks/useGetFirestoreData"
+import DataLoading from "@/components/DataLoading"
 
 const choresParent = () => {
+  const parentID = useGetUserID()
+  const parent = useGetUser(parentID.data || "")
+  const children = useGetChildren(parent.data?.children || [])
+
+  const [selectedChildIndex, setSelectedChildIndex] = useState(0)
+
+  const selectedChildID = parent.data?.children?.[selectedChildIndex]
+
+  if (children.some((query) => query.isPending)) {
+    return <DataLoading />
+  }
   return (
     <SafeAreaView className="bg-white h-full">
       <AppHeader parent />
-      <Text>choresParent</Text>
+      <View className="flex items-center mt-6">
+        <FlatList
+          scrollEnabled={true}
+          horizontal={true}
+          data={children.map((child) => child.data)}
+          renderItem={({ item, index }) => {
+            const isSelected = selectedChildIndex === index
+            return (
+              <Pressable
+                className="flex flex-col items-center justify-center gap-4"
+                onPress={() => setSelectedChildIndex(index)}
+              >
+                <View
+                  className={`rounded-full h-20 w-20 justify-center items-center overflow-hidden ${isSelected ? "border-4 border-blue-500" : ""}`}
+                >
+                  <Image
+                    source={{ uri: item?.profilePicture }}
+                    className="w-full h-full"
+                    style={{ resizeMode: "cover" }}
+                  />
+                </View>
+                <Text>{item?.name}</Text>
+              </Pressable>
+            )
+          }}
+          keyExtractor={(item) => item?.name || ""}
+          showsHorizontalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View className="w-8" />}
+        />
+      </View>
     </SafeAreaView>
   )
 }
---
+++ backend/types/savingGoal.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+//The type as it is defined in Firestore
+export const SavingGoalSchema = z.object({
+  // This field is optional because we want to use this schema for both creating and updating saving goals.
+  // We should look at creating different schemas for these two operations as this is better practice.
+  id: z.string().optional(),
+  //account_id: z.string(),
+  child_id: z.string(),
+  current_amount: z.number(),
+  goal_amount: z.number(),
+  icon_id: z.string(),
+  title: z.string(),
+});
+
+export type SavingGoal = z.infer<typeof SavingGoalSchema>;
