bbb7badfc4554d4b5d21166ba984c4ba3c90d43e
Andreas Tande <andretan@stud.ntnu.no>
2024-11-20 13:24:13 +0100

    Fixed coloring of deadline for chores

--- components/chores/chore.tsx
+++ components/chores/chore.tsx
@@ -8,10 +8,16 @@ interface Props {
 }
 
 const ChoreList: React.FC<Props> = ({ chore, onClick }) => {
-  const urgent =
-    new Date(chore.time_limit.seconds * 1000) < new Date(new Date().setDate(new Date().getDate() + 3))
-      ? "text-red-500"
-      : "text-black"
+  const choreDueDate = new Date(chore.time_limit.seconds * 1000)
+  const currentTime = new Date()
+  const threeDaysFromNow = new Date().setDate(currentTime.getDate() + 3)
+
+  const color =
+    choreDueDate < currentTime
+      ? "text-red-500" // Past due
+      : choreDueDate.getTime() < threeDaysFromNow
+        ? "text-yellow-600" // Due within 3 days
+        : "text-black" // More than 3 days left
 
   const availableChore = () => {
     return (
@@ -22,7 +28,7 @@ const ChoreList: React.FC<Props> = ({ chore, onClick }) => {
               <Text className="text-base">{chore.chore_title}</Text>
               <View className="flex-row items-center space-x-1.5">
                 <Text className="text-xs color-slate-400">Frist:</Text>
-                <Text className={urgent}>
+                <Text className={color}>
                   {new Date(chore.time_limit.seconds * 1000).toLocaleDateString("en-GB", {
                     day: "2-digit",
                     month: "2-digit",
@@ -42,7 +48,12 @@ const ChoreList: React.FC<Props> = ({ chore, onClick }) => {
   }
 
   const unavailablechore = () => {
-    const colour = chore.chore_status === "complete" ? "text-grenn-600" : chore.chore_status === "rejected" ? "text-red-600" : "text-yellow-600";
+    const colour =
+      chore.chore_status === "complete"
+        ? "text-green-600"
+        : chore.chore_status === "rejected"
+          ? "text-red-600"
+          : "text-yellow-600"
 
     return (
       <Pressable onPress={onClick}>
